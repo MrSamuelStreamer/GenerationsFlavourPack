6eced30cd1c102709135107c82edd0a3656c2aa7
Kieran David Evans <keyz182@gmail.com>
2024-11-15 14:36:35 +0000

    Cookie cutter initial commit

---
+++ 1.5/Source/MrSamuelStreamerGenerationsFlavourPack/MSS_GenDefOf.cs
@@ -0,0 +1,14 @@
+﻿using RimWorld;
+using Verse;
+
+namespace MSS_Gen;
+
+[DefOf]
+public static class MSS_GenDefOf
+{
+    // Remember to annotate any Defs that require a DLC as needed e.g.
+    // [MayRequireBiotech]
+    // public static GeneDef YourPrefix_YourGeneDefName;
+
+    static MSS_GenDefOf() => DefOfHelper.EnsureInitializedInCtor(typeof(MSS_GenDefOf));
+}
---
+++ 1.5/Source/MrSamuelStreamerGenerationsFlavourPack/MSS_GenMod.cs
@@ -0,0 +1,34 @@
+﻿using Verse;
+using UnityEngine;
+using HarmonyLib;
+
+namespace MSS_Gen;
+
+public class MSS_GenMod : Mod
+{
+    public static Settings settings;
+
+    public MSS_GenMod(ModContentPack content) : base(content)
+    {
+        ModLog.Debug("Hello world from Mr Samuel Streamer's Generations Flavour Pack");
+
+        // initialize settings
+        settings = GetSettings<Settings>();
+#if DEBUG
+        Harmony.DEBUG = true;
+#endif
+        Harmony harmony = new Harmony("MrSamuelStreamer.rimworld.MSS_Gen.main");
+        harmony.PatchAll();
+    }
+
+    public override void DoSettingsWindowContents(Rect inRect)
+    {
+        base.DoSettingsWindowContents(inRect);
+        settings.DoWindowContents(inRect);
+    }
+
+    public override string SettingsCategory()
+    {
+        return "MSS_Gen_SettingsCategory".Translate();
+    }
+}
