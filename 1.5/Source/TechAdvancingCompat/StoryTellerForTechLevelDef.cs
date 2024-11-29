31550f89d0c74c801d231261c6d050dbbab802ae
Kieran David Evans <keyz182@gmail.com>
2024-11-29 15:56:31 +0000

    Def to allow setting storyteller per tech level

---
+++ 1.5/Source/TechAdvancingCompat/StoryTellerForTechLevelDef.cs
@@ -0,0 +1,10 @@
+﻿using RimWorld;
+using Verse;
+
+namespace MSS_Gen.TechAdvancingCompat;
+
+public class StoryTellerForTechLevelDef: Def
+{
+    public StorytellerDef StoryTeller;
+    public TechLevel TechLevel;
+}
---
+++ 1.5/Source/TechAdvancingCompat/StoryTellerTechLevelWorldComponent.cs
@@ -0,0 +1,30 @@
+﻿using RimWorld;
+using RimWorld.Planet;
+using Verse;
+
+namespace MSS_Gen.TechAdvancingCompat;
+
+public class StoryTellerTechLevelWorldComponent(World world) : WorldComponent(world), ISignalReceiver
+{
+    public override void FinalizeInit()
+    {
+        base.FinalizeInit();
+        Find.SignalManager.RegisterReceiver(this);
+    }
+
+    public void Notify_SignalReceived(Signal signal)
+    {
+        if(signal.tag != Signals.MSS_Gen_TechLevelChanged) return;
+
+        TechLevel? techLevel = (TechLevel)signal.args.GetArg(0).arg;
+
+        StoryTellerForTechLevelDef newTeller = DefDatabase<StoryTellerForTechLevelDef>.AllDefsListForReading.FirstOrDefault(def => def.StoryTeller!=null && def.TechLevel == techLevel);
+
+        if(newTeller == null) return;
+
+        Current.Game.storyteller.def = newTeller.StoryTeller;
+        Current.Game.storyteller.Notify_DefChanged();
+
+        ModLog.Log($"Storyteller changed to {newTeller.StoryTeller.LabelCap}");
+    }
+}
