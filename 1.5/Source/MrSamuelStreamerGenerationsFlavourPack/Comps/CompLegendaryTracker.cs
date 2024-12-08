da995732cf26a989cbf317cc9053ba35f8c81167
Kieran David Evans <keyz182@gmail.com>
2024-12-08 13:04:46 +0000

    Start work on the legendary tracking

---
+++ 1.5/Source/MrSamuelStreamerGenerationsFlavourPack/Comps/CompLegendaryTracker.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections.Generic;
+using Verse;
+
+namespace MSS_Gen.Comps;
+
+public class CompLegendaryTracker: ThingComp
+{
+    public class PawnUsageTracker: IExposable
+    {
+        public int ticksEquipped = 0;
+        public int timesUsed = 0;
+        public bool diedWhileEquipped = false;
+
+        public int LastEquippedAt = -1;
+        public int LastUnequippedAt = -1;
+
+        public int Kills = 0;
+
+        public void ExposeData()
+        {
+            Scribe_Values.Look(ref ticksEquipped, "ticksEquipped");
+            Scribe_Values.Look(ref timesUsed, "timesUsed");
+            Scribe_Values.Look(ref diedWhileEquipped, "diedWhileEquipped");
+        }
+    }
+
+    public Dictionary<Pawn, PawnUsageTracker> pawnUsageTrackers = new();
+
+    public override void PostExposeData()
+    {
+        base.PostExposeData();
+        Scribe_Collections.Look(ref pawnUsageTrackers, "pawnUsageTrackers", LookMode.Reference, LookMode.Value);
+    }
+
+    public PawnUsageTracker TrackerForPawn(Pawn pawn)
+    {
+        if (!pawnUsageTrackers.TryGetValue(pawn, out PawnUsageTracker tracker))
+        {
+            tracker = new PawnUsageTracker();
+            pawnUsageTrackers.Add(pawn, tracker);
+        }
+
+        return tracker;
+    }
+
+    public void AddToEquippedTime(PawnUsageTracker tracker)
+    {
+        if (tracker.LastEquippedAt > 0 && tracker.LastUnequippedAt > 0 && tracker.LastEquippedAt < tracker.LastUnequippedAt)
+        {
+            tracker.ticksEquipped += tracker.LastUnequippedAt - tracker.LastEquippedAt;
+
+            tracker.LastEquippedAt = -1;
+        }
+    }
+
+    public override void Notify_Equipped(Pawn pawn)
+    {
+        base.Notify_Equipped(pawn);
+        PawnUsageTracker tracker = TrackerForPawn(pawn);
+
+        AddToEquippedTime(tracker);
+
+        tracker.LastEquippedAt = Find.TickManager.TicksGame;
+    }
+
+    public override void Notify_Unequipped(Pawn pawn)
+    {
+        base.Notify_Unequipped(pawn);
+        PawnUsageTracker tracker = TrackerForPawn(pawn);
+
+        TrackerForPawn(pawn).LastUnequippedAt = Find.TickManager.TicksGame;
+        AddToEquippedTime(tracker);
+    }
+
+    public override void Notify_UsedVerb(Pawn pawn, Verb verb)
+    {
+        base.Notify_UsedVerb(pawn, verb);
+        TrackerForPawn(pawn).timesUsed++;
+    }
+
+    public override void Notify_UsedWeapon(Pawn pawn)
+    {
+        base.Notify_UsedWeapon(pawn);
+        TrackerForPawn(pawn).timesUsed++;
+    }
+
+    public override void Notify_KilledPawn(Pawn pawn)
+    {
+        base.Notify_KilledPawn(pawn);
+        TrackerForPawn(pawn).Kills++;
+    }
+
+    public override void Notify_WearerDied()
+    {
+        base.Notify_WearerDied();
+        if (parent.ParentHolder is Pawn_EquipmentTracker equipmentTracker)
+        {
+            TrackerForPawn(equipmentTracker.pawn).diedWhileEquipped = true;
+        }
+    }
+}
