ad5b15afce2adc6b92cc694e1f7864d7f263b8c1
Kieran David Evans <keyz182@gmail.com>
2024-12-13 13:58:57 +0000

    Daily romance attempts

---
+++ 1.5/Source/MrSamuelStreamerGenerationsFlavourPack/HarmonyPatches/InteractionWorker_RomanceAttempt_Patch.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Reflection.Emit;
+using HarmonyLib;
+using RimWorld;
+
+namespace MSS_Gen.HarmonyPatches;
+
+[HarmonyPatch(typeof(InteractionWorker_RomanceAttempt))]
+public static class InteractionWorker_RomanceAttempt_Patch
+{
+    [HarmonyPatch(nameof(InteractionWorker_RomanceAttempt.Interacted))]
+    [HarmonyTranspiler]
+    public static IEnumerable<CodeInstruction> InteractedTranspiler(IEnumerable<CodeInstruction> instructions)
+    {
+        foreach (CodeInstruction instruction in instructions)
+        {
+            if (instruction.opcode == OpCodes.Ldc_I4 && (int) instruction.operand == 900000)
+            {
+                instruction.operand = GenDate.TicksPerDay;
+            }
+            yield return instruction;;
+        }
+    }
+}
