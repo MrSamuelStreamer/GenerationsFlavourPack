65f59c064086fb73818e74ba731ccf38023d82d9
Kieran David Evans <keyz182@gmail.com>
2024-12-13 14:42:45 +0000

    Hide tribals advance gizmo

---
+++ 1.5/Source/TechAdvancingCompat/HarmonyPatches/RitualObligationTargetWorker_AnyGatherSpotForAdvancement_Patch.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using RimWorld;
+using VFETribals;
+
+namespace MSS_Gen.TechAdvancingCompat.HarmonyPatches;
+
+[HarmonyPatch(typeof(RitualObligationTargetWorker_AnyGatherSpot))]
+public static class RitualObligationTargetWorker_AnyGatherSpotForAdvancement_Patch
+{
+    [HarmonyPatch("CanUseTargetInternal")]
+    [HarmonyPostfix]
+    public static void CanUseTargetInternal(RitualObligationTargetWorker_AnyGatherSpot __instance, ref RitualTargetUseReport __result)
+    {
+        if(__instance is RitualObligationTargetWorker_AnyGatherSpotForAdvancement)
+            __result.canUse = false;
+    }
+}
