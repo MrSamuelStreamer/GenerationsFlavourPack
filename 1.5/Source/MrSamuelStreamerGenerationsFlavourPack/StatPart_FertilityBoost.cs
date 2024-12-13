b6cc56d5ee6308e613b7859099516f769292f7ad
Kieran David Evans <keyz182@gmail.com>
2024-12-13 14:14:44 +0000

    Fertility boost

---
+++ 1.5/Source/MrSamuelStreamerGenerationsFlavourPack/StatPart_FertilityBoost.cs
@@ -0,0 +1,27 @@
+﻿using RimWorld;
+using Verse;
+
+namespace MSS_Gen;
+
+public class StatPart_FertilityBoost: StatPart
+{
+    public float factor = .5f;
+
+    public override void TransformValue(StatRequest req, ref float val)
+    {
+        if (req.Thing is Pawn pawn)
+        {
+            val += factor;
+        }
+    }
+
+    public override string ExplanationPart(StatRequest req)
+    {
+        if (req.Thing is Pawn pawn)
+        {
+            return "MSS_Gen_StatPart_FertilityBoost_Explanation".Translate(factor.ToStringPercentSigned()).CapitalizeFirst();
+        }
+
+        return null;
+    }
+}
