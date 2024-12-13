948276996439eee58d8db91464bb34d8004a0b3d
Kieran David Evans <keyz182@gmail.com>
2024-12-13 13:25:37 +0000

    Only wildmen from ritual
    Closes #33

---
+++ 1.5/Source/TechAdvancingCompat/HarmonyPatches/RitualOutcomeEffectWorker_TribalGathering_Patch.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Reflection.Emit;
+using HarmonyLib;
+using UnityEngine;
+using VFETribals;
+
+namespace MSS_Gen.TechAdvancingCompat.HarmonyPatches;
+
+[HarmonyPatch(typeof(RitualOutcomeEffectWorker_TribalGathering))]
+public static class RitualOutcomeEffectWorker_TribalGathering_Patch
+{
+    [HarmonyPatch(nameof(RitualOutcomeEffectWorker_TribalGathering.Apply))]
+    [HarmonyTranspiler]
+    public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+    {
+        bool first = false;
+        bool second = false;
+        bool third = false;
+
+        foreach (CodeInstruction instruction in instructions)
+        {
+            if (!first && instruction.opcode == OpCodes.Ldc_R4 && Mathf.Approximately((float) (instruction.operand), 0.5f))
+            {
+                first = true;
+                instruction.operand = 0f;
+            }else if (!second && instruction.opcode == OpCodes.Ldc_R4 && Mathf.Approximately((float) (instruction.operand), 0.25f))
+            {
+                second = true;
+                instruction.operand = 0f;
+            }else if (!third && instruction.opcode == OpCodes.Ldc_R4 && Mathf.Approximately((float) (instruction.operand), 0.5f))
+            {
+                third = true;
+                instruction.operand = 0f;
+            }
+
+            yield return instruction;
+        }
+    }
+}
