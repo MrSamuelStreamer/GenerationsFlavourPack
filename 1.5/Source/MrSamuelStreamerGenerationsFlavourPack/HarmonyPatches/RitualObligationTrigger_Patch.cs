3b13fb0e28655502b07590a493b167ef0446f33d
Kieran David Evans <keyz182@gmail.com>
2024-12-23 11:28:08 +0000

    Move era advancement to post-ritual

--- 1.5/Source/MrSamuelStreamerGenerationsFlavourPack/HarmonyPatches/RitualObligationTrigger_Patch.cs
+++ 1.5/Source/MrSamuelStreamerGenerationsFlavourPack/HarmonyPatches/RitualObligationTrigger_Patch.cs
@@ -1,20 +1,27 @@
-﻿using HarmonyLib;
+﻿using System.Linq;
+using HarmonyLib;
 using RimWorld;
 using Verse;
 using VFETribals;
 
 namespace MSS_Gen.HarmonyPatches;
 
-[HarmonyPatch(typeof(GameComponent_Tribals))]
-public static class GameComponent_Tribals_Patch
+[HarmonyPatch(typeof(RitualObligationTrigger))]
+public static class RitualObligationTrigger_Patch
 {
-    [HarmonyPatch(nameof(GameComponent_Tribals.AdvanceToEra))]
+    [HarmonyPatch(nameof(RitualObligationTrigger.Notify_RitualExecuted))]
     [HarmonyPostfix]
-    public static void AdvanceToEraPostfix(EraAdvancementDef def)
+    public static void Notify_RitualExecuted(RitualObligationTrigger __instance, LordJob_Ritual ritual)
     {
+        if(__instance is not RitualObligationTrigger_TargetTechlevel ritualObligationTrigger) return;
+
         TechConfigWorldComponent comp = Find.World.GetComponent<TechConfigWorldComponent>();
 
         if(comp == null) return;
+
+        EraAdvancementDef def = DefDatabase<EraAdvancementDef>.AllDefs.FirstOrDefault(def => def.newTechLevel == ritualObligationTrigger.targetTechLevel);
+
+        if(def == null) return;
 
         if (def == VFET_DefOf.VFET_FormTribe)
         {
