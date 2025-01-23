68ad65b1a52b0b700afeac83b9f4083f972bb443
Kieran David Evans <keyz182@gmail.com>
2025-01-23 15:39:23 +0000

    Stop old people getting pomegranate

--- 1.5/Source/MrSamuelStreamerGenerationsFlavourPack/StatPart_FertilityBoost.cs
+++ 1.5/Source/MrSamuelStreamerGenerationsFlavourPack/StatPart_FertilityBoost.cs
@@ -6,12 +6,21 @@ namespace MSS_Gen;
 public class StatPart_FertilityBoost: StatPart
 {
     public float factor = .5f;
+    public float cutOff = 0.625f;
+    public float cutOffFactor = -1f;
 
     public override void TransformValue(StatRequest req, ref float val)
     {
         if (req.Thing is Pawn pawn && val > 0f)
         {
-            val += factor;
+            if (pawn.ageTracker.AgeBiologicalYears < pawn.RaceProps.lifeExpectancy * cutOff)
+            {
+                val += factor;
+            }
+            else
+            {
+                val += cutOffFactor;
+            }
         }
     }
 
